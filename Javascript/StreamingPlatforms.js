a1e8b01d3057870fdd799b4c2edfa6b44be1e655
David Gonzalez <dgonz8992@gmail.com>
2026-09-02 22:55:04 -0400

    added stuff

--- Javascript/StreamingPlatforms.js
+++ Javascript/StreamingPlatforms.js
@@ -1,19 +1,43 @@
-const openButton = document.getElementById("openStreamingPlatforms");
-const closeButton = document.getElementById("closeStreamingPlatforms");
-const popup = document.getElementById("streamingPlatformsPopup");
+const openStreaming = document.getElementById("openStreamingPlatforms");
+const closeStreaming = document.getElementById("closeStreamingPlatforms");
+const popupStreaming = document.getElementById("streamingPlatformsPopup");
 
-openButton.addEventListener("click", () => {
-    popup.showModal()
+openStreaming.addEventListener("click", () => {
+    popupStreaming.showModal()
     document.body.style.overflow = "hidden";
 });
 
-closeButton.addEventListener("click", () => {
-    popup.close();
+closeStreaming.addEventListener("click", () => {
+    popupStreaming.close();
     document.body.style.overflow = "";
 });
 
-popup.addEventListener("click", (event) => {
-    if (event.target === popup) {
-        popup.close();
+popupStreaming.addEventListener("click", (event) => {
+    if (event.target === popupStreaming) {
+        popupStreaming.close();
+        document.body.style.overflow = "";
     }
 });
+
+
+const openSocials = document.getElementById("openSocials")
+const closeSocials = document.getElementById("closeSocials")
+const socialsPopup = document.getElementById("socialsPopup")
+
+openSocials.addEventListener("click", () => {
+    socialsPopup.showModal()
+    document.body.style.overflow = "hidden";
+});
+
+closeSocials.addEventListener("click", () => {
+    socialsPopup.close();
+    document.body.style.overflow = "";
+});
+
+socialsPopup.addEventListener("click", (event) => {
+    if (event.target === socialsPopup) {
+        socialsPopup.close();
+        document.body.style.overflow = "";
+    }
+});
+
